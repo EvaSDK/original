3177cc2dd49888474299adf99a25298b4b9bda21
Gilles Dartiguelongue <gilles.dartiguelongue@esiee.org>
2015-07-14 19:40:22 +0200

    Send pictures using Flask
    Avoids hassle of configuring a separate web server for developement.
    Need to revisit this for performance later.

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,8 @@
 * Fix comment submission anti-spam validation.
 * Fix bypassing of locked galleries via direct links.
 * Fix typo in MQ picture check.
+* Send pictures using Flask and kill all hardcoded domain name
+  references.
 
 1.0.0 (2015-03-16)
 ------------------
--- original/wsgi.py
+++ original/wsgi.py
@@ -2,15 +2,23 @@
 
 """ Help running application. """
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask.ext.babel import Babel
 
 app = Flask(__name__.split('.')[0])
 babel = Babel(app)
 
+from original import gallery
 from original.views.gallery import GalleryView
 
 GalleryView.register(app)
+
+
+@app.route('/galleries/<path:path>')
+def send_pic(path):
+    """Send pictures from picture folder."""
+    return send_from_directory(gallery.PHOTO_ROOT, path)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5004, debug=True)
