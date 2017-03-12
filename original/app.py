a53f3b057b1a8ca7e27ec15059b8a7cf40ece86c
Gilles Dartiguelongue <gilles.dartiguelongue@esiee.org>
2017-03-12 12:08:10 +0100

    Use context processor to fix footer data generation

--- original/app.py
+++ original/app.py
@@ -2,11 +2,14 @@
 
 """Application factory."""
 
+import datetime
 import os
 
+from babel.dates import get_timezone
 from flask import Flask
 from flask_babel import Babel, format_date, format_datetime
 
+import original
 from original.views import get_locale, send_pic
 from original.views.gallery import (
     GalleryDetailView,
@@ -42,8 +45,11 @@ def create_app(config=None):
     app.jinja_env.filters['date'] = format_date
     app.jinja_env.filters['datetime'] = format_datetime
 
+    app.context_processor(app_context_processor)
+
     GalleryListView.register(app)
     GalleryDetailView.register(app)
+
     return app
 
 
@@ -51,3 +57,11 @@ def app_factory(global_config, **local_conf):
     """PasteDeploy WSGI application factory entry-point."""
     app = create_app(config=local_conf)
     return app
+
+
+def app_context_processor():
+    """Extend context with some application data."""
+    return {
+        'now': datetime.datetime.now(tz=get_timezone()),
+        'version': original.__version__,
+    }
